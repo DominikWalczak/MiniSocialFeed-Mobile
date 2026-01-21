4cdd8f3a6b761bde78f3c79f8ef5628cc3e12c74
Dominik Walczak <dominik.walczak2@gmail.com>
2026-01-21 12:54:43 +0100

    usuwam console.log służące do debuggowania

--- app/login.tsx
+++ app/login.tsx
@@ -52,8 +52,6 @@ export default function Login() {
           })),
         }
       }
-      console.log(data)
-      console.log(dataCheck)
       saveAccessToken(dataCheck.data.data.accessToken)
       saveRefreshToken(dataCheck.data.data.refreshToken)
       navigation.navigate('User');
